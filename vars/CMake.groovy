6b0051ff08541c06badc6e49d03e868d49d44be2
davidjsherman <david.sherman@inria.fr>
2016-09-22 21:48:39 +0200

    combined changes

--- vars/CMake.groovy
+++ vars/CMake.groovy
@@ -4,57 +4,53 @@
 
 def call(body) {
 
-	// body.buildDir
-	// body.sourceDir
-	// body.buildType
-	// body.installDir
-	// body.getArguments
-	// body.getGenerator
-	// body.preloadScript
-	// body.getCmakeArgs
+  workDir = pwd()
 
-	//node {
-		workDir = pwd()
-		try {
-			getCmakeArgs = body.getCmakeArgs.join(' ')
-		} catch (e) {
-			getCmakeArgs = body.getCmakeArgs
-		}
-		try {
-			getArguments = body.getArguments.join(' ')
-		} catch (e) {
-			getArguments = body.getArguments
-		}
-		withEnv(["buildDir=${body.buildDir ?: '_build'}",
-			"sourceDir=${body.sourceDir ?: workDir}",
-			"buildType=${body.buildType ?: 'Debug'}",
-			"installDir=${body.installDir ?: '_install'}",
-			"getArguments=${body.getArguments ?: ''}",
-			"getGenerator=${body.getGenerator ?: 'Unix Makefiles'}",
-			"preloadScript=${body.preloadScript ?: ''}",
-			"getCmakeArgs=${getCmakeArgs ?: ''}",
-			"workDir=${workDir}"]) {
+  cleanBuild = body.cleanBuild ?: true
+  buildDir = body.buildDir ?: workDir + '/_build'
+  sourceDir = body.sourceDir ?: workDir
+  
+  envs = (body.envs instanceof ArrayList ? body.envs : [])
+  envs << "getCmakeArgs=" + (body.getCmakeArgs instanceof String[] ? body.getCmakeArgs.join(' ') : (body.getCmakeArgs ?: ''))
+  envs << "getArguments=" + (body.getArguments instanceof String[] ? body.getArguments.join(' ') : (body.getArguments ?: ''))
+  envs << "buildDir=" + buildDir // (body.buildDir ?: workDir + '/_build')
+  envs << "sourceDir=" + sourceDir // (body.sourceDir ?: workDir)
+  envs << "buildType=" + (body.buildType ?: 'Debug')
+  envs << "installDir=" + (body.installDir ?: workdir + '/_install')
+  envs << "getGenerator=" + (body.getGenerator ?: 'Unix Makefiles')
+  envs << "workDir=" + (workDir)
 
-			sh '''echo run CMake with parameters:
-				echo "workDir = ${workDir}"
-				echo "getArguments = ${getArguments}"
-				echo "buildDir = ${buildDir}"
-				echo "sourceDir = ${sourceDir}"
-				echo "getGenerator = ${getGenerator}"
-				echo "preloadScript = ${preloadScript}"
-				echo "buildType = ${buildType}"
-				echo "getCmakeArgs = ${getCmakeArgs}"
-				mkdir -p "$installDir" "$buildDir"
-				cd "$buildDir"
-				cmake "$sourceDir" -G "$getGenerator" $getArguments \
-					-DCMAKE_INSTALL_PREFIX:PATH="$installDir" \
-					-DCMAKE_BUILD_TYPE:STRING="$buildType" \
-					$getCmakeArgs
-				make
-				make install
-			'''
-		}
-	//}
+  preloadScript = (body.preloadScript instanceof ArrayList ? body.preloadScript.join(" ") : body.preloadScript) ?: ''
+		
+  script = new String('''#!/bin/bash -l
+	echo -e run CMake with parameters: "\\n"workDir = ${workDir}"\\n"getArguments = ${getArguments}"\\n"buildDir = ${buildDir}"\\n"sourceDir = ${sourceDir}"\\n"getGenerator = ${getGenerator}"\\n"preloadScript = ${preloadScript}"\\n"buildType = ${buildType}"\\n"getCmakeArgs = ${getCmakeArgs}
+'''.stripIndent())
+
+  if (cleanBuild && buildDir != sourceDir) {
+    script += "rm -rf \"$buildDir\"\n"
+  }
+
+  script += '''
+	mkdir -p "$installDir" "$buildDir"
+	cd "$buildDir"
+	'''.stripIndent()
+
+  if (body.preloadScript) {
+    script += preloadScript + "\n"
+  }
+
+  script += '''
+	cmake "$sourceDir" -G "$getGenerator" $getArguments \\
+		-DCMAKE_INSTALL_PREFIX:PATH="$installDir" \\
+		-DCMAKE_BUILD_TYPE:STRING="$buildType" \\
+		$getCmakeArgs
+	make
+	make install
+	'''.stripIndent()
+
+  withEnv(envs) {
+    sh script
+  }
 }
 
 return this;
