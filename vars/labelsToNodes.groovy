305b4ad55b12dedf9d6f02ed0d5eb05f04defd86
davidjsherman <david.sherman@inria.fr>
2016-10-08 08:28:56 +0200

    update

--- vars/labelsToNodes.groovy
+++ vars/labelsToNodes.groovy
@@ -7,20 +7,20 @@ def build(body) {
   def labels = body.labels
   for (x in labels) {
     def label = x
-    nodesMap[label] = {
-      node(label) {
-	//unstash 'source'
-	CMake([buildType: 'Debug',
-	       sourceDir: '$workDir/', // + component,
-	       buildDir: '$workDir/_build/', // + component + '/' + label,
-	       installDir: '$workDir/_install/', // + label,
-	       getCmakeArgs: [ '-DBUILD_SHARED_LIBS:BOOL=ON' ]
-	      ])
-	script {
-	  env.dashel_DIR = sh ( script: 'dirname $(find _install -name dashelConfig.cmake | head -1)', returnStdout: true).trim()
-	}
-      }
-    }
+    // nodesMap[label] = {
+    //   node(label) {
+    // 	//unstash 'source'
+    // 	CMake([buildType: 'Debug',
+    // 	       sourceDir: '$workDir/', // + component,
+    // 	       buildDir: '$workDir/_build/', // + component + '/' + label,
+    // 	       installDir: '$workDir/_install/', // + label,
+    // 	       getCmakeArgs: [ '-DBUILD_SHARED_LIBS:BOOL=ON' ]
+    // 	      ])
+    // 	script {
+    // 	  env.dashel_DIR = sh ( script: 'dirname $(find _install -name dashelConfig.cmake | head -1)', returnStdout: true).trim()
+    // 	}
+    //   }
+    // }
   }
   println(nodesMap);
   return nodesMap;
